9d08661204cf55323af92f45d512091146ac9583
Giulio Mori <mori.giulio.94@gmail.com>
2023-05-31 11:59:40 +0000

    final edits

--- experience_manager.py
+++ experience_manager.py
@@ -1,8 +1,8 @@
-import os
-import sys
 import sys, os
+from player_model import PlayerModel
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'EV_PDDL'))
 from platform_communication import PlatformCommunication
+from encounter import Encounter
 import time
 import jsonpickle
 from ev_pddl.PDDL import PDDL_Parser
@@ -10,8 +10,8 @@ from ev_pddl.world_state import WorldState
 from ev_pddl.action import Action
 import input_timeout
 import threading
-import json
 import debugpy
+import random
 
 class ExperienceManager:
 
@@ -21,6 +21,8 @@ class ExperienceManager:
     def __init__(self) -> None:
         self.platform_communication = PlatformCommunication()
         self._PDDL_parser = PDDL_Parser()
+        self.encounters = []
+        self.player_model = PlayerModel()
 
     def start_platform_communication(self):
         """
@@ -53,6 +55,8 @@ class ExperienceManager:
             print("Domain received successfully")
             self.PDDL_problem_text = str(response['problem'])
             print("Problem received successfully")
+            self.encounters_received = jsonpickle.decode(str(response['additional_data']))
+            print("Encounters received successfully")
             print("Handshake -- phase 3 successful.")
         else:
             raise Exception("Error: Received unexpected message: " + response['text'])
@@ -90,8 +94,6 @@ class ExperienceManager:
         This is the main loop of the experience manager.
         """
         self.platform_communication.start_receiving_messages()
-        # debugpy.listen(5678)
-        # debugpy.wait_for_client()
         self.domain = self._PDDL_parser.parse_domain(domain_str = self.PDDL_domain_text)
         # self.domain = self._PDDL_parser.parse_domain(domain_filename="domain.pddl")
         print("Domain parsed correcly")
@@ -100,9 +102,12 @@ class ExperienceManager:
         print("Problem parsed correcly")
         self.environment_state = WorldState()
         self.environment_state.create_worldstate_from_problem(problem = self.problem, domain=self.domain)
+        for item in self.encounters_received['encounters']:
+            self.encounter_initialization(item)
         print("Starting normal communication...")
         print("Press something to start the action builder...")
         thread = None
+        # debugpy.listen(5678)
         while True:
             message = self.platform_communication.get_received_message()
             if message is not None:
@@ -113,29 +118,86 @@ class ExperienceManager:
                 if thread is None or thread.is_alive() == False:
                     print("Received message: " + str(changed_relations))
                 self.update_environment_state(changed_relations)
-            if thread is None or thread.is_alive() == False:
-                try:
-                    answer = input_timeout.input_with_timeout("", 1)
-                except input_timeout.TimeoutExpired:
-                    pass
-                else:
-                    thread = threading.Thread(target=self.create_action_to_send_to_environment, daemon=True)
-                    thread.start()
+                applicable_encounters = self.get_available_encounters()
+                if len(applicable_encounters) == 1:
+                    message = applicable_encounters[0].get_start_encouter_message()
+                    self.platform_communication.send_message(message)
+                    applicable_encounters[0].executed = True
+                if len(applicable_encounters) > 1:
+                    encounter = self.get_random_encounter(applicable_encounters)
+                    self.flag_other_encouters(applicable_encounters, encounter)
+                    message = encounter.get_start_encouter_message()
+                    self.platform_communication.send_message(message)
+                    encounter.executed = True
+                    time.sleep(1)
+            # if thread is None or thread.is_alive() == False:
+            #     try:
+            #         answer = input_timeout.input_with_timeout("", 1)
+            #     except input_timeout.TimeoutExpired:
+            #         pass
+            #     else:
+            #         thread = threading.Thread(target=self.create_action_to_send_to_environment, daemon=True)
+            #         thread.start()
     
+    def get_available_encounters(self) -> list[Encounter]:
+        """
+        This method is used to get the encounters that are available to be applied. 
+        It checks the preconditions of the encounters with the worldstate and returns the ones that can be applied.
+        """
+        return_list = []
+        for encounter in self.encounters:
+            if encounter.executed == False and encounter.skipped == False:
+                applicable, _ = self.environment_state.check_precondition_recursive(encounter.preconditions)
+                if applicable:
+                    return_list.append(encounter)
+        return return_list
+
+    def get_random_encounter(self, available_encouters: list[Encounter]) -> Encounter:
+        """
+        This method is used to get the most suited encounter based on the player model.
+        """
+        # debugpy.breakpoint()
+        return available_encouters[random.randint(0, len(available_encouters) - 1)]
+
+    def flag_other_encouters(self, available_encounters: list[Encounter], encounter_to_skip: Encounter):
+        """
+        This method is used to skip the other encounters.
+        """
+        for encounter in available_encounters:
+            if encounter != encounter_to_skip:
+                encounter.skipped = True
+
+    def encounter_initialization(self, encounter_data):
+        """
+        This method is used to initialize the encounter.
+        """
+        encounter = Encounter(encounter_data, self.environment_state)
+        self.encounters.append(encounter)
+
     def update_environment_state(self, changed_relations):
         """
         This method is used to update the environment state.
         """
         for item in changed_relations:
-            for rel in item:
-                if rel[0] == 'new':
-                    self.environment_state.add_relation_from_PDDL(rel[1])
-                elif rel[0] == 'changed_value':
-                    PDDL_relation = self.environment_state.create_relation_from_PDDL(rel[1])
-                    environment_state_relation = self.environment_state.find_relation(relation = PDDL_relation, exclude_value = True)
-                    environment_state_relation.modify_value(PDDL_relation.value)
-                elif rel[0] == 'new_entity':
-                    self.environment_state.add_entity_from_PDDL(rel[1])
+            if len(item) > 0 and item[0] == 'update_player_model':
+                print("Updating player model." + str(item[1]))
+                self.update_player_model(item[1])
+            else:
+                for rel in item:
+                    if rel[0] == 'new':
+                        self.environment_state.add_relation_from_PDDL(rel[1])
+                    elif rel[0] == 'changed_value':
+                        PDDL_relation = self.environment_state.create_relation_from_PDDL(rel[1])
+                        environment_state_relation = self.environment_state.find_relation(relation = PDDL_relation, exclude_value = True)
+                        environment_state_relation.modify_value(PDDL_relation.value)
+                    elif rel[0] == 'new_entity':
+                        self.environment_state.add_entity_from_PDDL(rel[1])
+
+    def update_player_model(self, player_model):
+        """
+        This method is used to update the player model.
+        """
+        self.player_model.update_player_model_from_message(player_model)
 
     def create_action_to_send_to_environment(self):
         """
